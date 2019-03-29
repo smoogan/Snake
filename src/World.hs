e91410a0ea83652a029b148908f64b04399c71d7
Sean Moogan <sean.b.moogan@gmail.com>
2019-03-28 23:12:00 -0400

    Adding comments

--- src/World.hs
+++ src/World.hs
@@ -4,7 +4,6 @@ import Snake
 import Debug.Trace
 import System.Random
 import Graphics.Gloss.Interface.IO.Game
-
 import Utils
 
 data World = World {
@@ -13,6 +12,7 @@ data World = World {
     food :: Point,
     gen :: StdGen
 } deriving (Show)
+
 
 createWorld :: StdGen -> World
 createWorld _g = World {
@@ -26,15 +26,19 @@ createWorld _g = World {
             dead = False },
         gen = _g }
 
--- TODO: Avoid posibility of food being placed on Snake
+
+-- Places the food on a random spot not occupied by Snake
 moveFood :: World -> World
 moveFood _world =
     let
+        -- Get a list of all unoccupied spots on the grid
         emptySpaces = [
             (x,y) |
                 x <- [-8..7],
                 y <- [-8..7],
                 not $ snakeOccupiesPoint (snake _world) (x,y) ]
+
+        -- Pick a random point from the list
         g0 = gen _world
         listLen = length emptySpaces
         (pointIndex, newGen) = randomR (0, listLen-1 :: Int) g0
@@ -42,6 +46,8 @@ moveFood _world =
     in
         _world { food = (x, y), gen = newGen }
 
+
+-- Move the world forward one iteration (i.e. Snake takes a step forward)
 stepWorld :: Float -> World -> World
 -- stepWorld _world | trace ("Stepping World: " ++ show (_world)) False = undefined
 stepWorld _time _world =
